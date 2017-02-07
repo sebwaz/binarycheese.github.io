4a8f4a498eaee8882d0048667bd888080690c271
Sebastian Waz <sebwaz@g.ucla.edu>
2017-02-06 23:37:04 -0800

    RHYTHM: change interface to 3:1/4:1 only
    get rid of middle button, prep score and counts so that it tracks only
    3:1 and 4:1 conditions

--- rhythm/main.js
+++ rhythm/main.js
@@ -21,11 +21,9 @@ var trial_num = -1;
 var ans_correct = 0;
 var block = [];
 
-var dup_counter = 0;
+// score counter
 var tri_counter = 0;
 var qua_counter = 0;
-
-var dup_correct = 0;
 var tri_correct = 0;
 var qua_correct = 0;
 
@@ -33,37 +31,19 @@ var show_scores = 0;
 
 function preload()
 {
-    dup_a = loadSound('samples/160/2-1.mp3');
-    tri_a = loadSound('samples/160/3-1.mp3');
-    qua_a = loadSound('samples/160/4-1.mp3');
-    
-    dup_b = loadSound('samples/180/2-1.mp3');
-    tri_b = loadSound('samples/180/3-1.mp3');
-    qua_b = loadSound('samples/180/4-1.mp3');
-    
-    dup_c = loadSound('samples/240/2-1.mp3');
-    tri_c = loadSound('samples/240/3-1.mp3');
-    qua_c = loadSound('samples/240/4-1.mp3');
-    
-    dup_d = loadSound('samples/320/2-1.mp3');
-    tri_d = loadSound('samples/320/3-1.mp3');
-    qua_d = loadSound('samples/320/4-1.mp3');
-    
-    dup_e = loadSound('samples/360/2-1.mp3');
-    tri_e = loadSound('samples/360/3-1.mp3');
-    qua_e = loadSound('samples/360/4-1.mp3');
-    
-    right_sound = loadSound('samples/feedback/right.mp3');
-    wrong_sound = loadSound('samples/feedback/wrong.mp3');
+    clv = loadSound('samples/clave.mp3');
+    rim = loadSound('samples/rimshot.mp3');
+    right_sound = loadSound('samples/right.mp3');
+    wrong_sound = loadSound('samples/wrong.mp3');
     
     play_img = loadImage('img/playing.jpg');
 }
 
 function knuth_shuffle(arr)
 {
-    for (trial = 0; trial < 15; trial++)
+    for (trial = 0; trial < 20; trial++)
     {
-        arr[trial] = trial;
+        arr[trial] = 3+(trial%2);
     }
     
     var cur_index = arr.length, temp_val, rand_index;
@@ -99,25 +79,8 @@ function setup()
 
     mode = 1;
     
-    dup_a.setVolume(wav_vol);
-    tri_a.setVolume(wav_vol);
-    qua_a.setVolume(wav_vol);
-    
-    dup_b.setVolume(wav_vol);
-    tri_b.setVolume(wav_vol);
-    qua_b.setVolume(wav_vol);
-    
-    dup_c.setVolume(wav_vol);
-    tri_c.setVolume(wav_vol);
-    qua_c.setVolume(wav_vol);
-    
-    dup_d.setVolume(wav_vol);
-    tri_d.setVolume(wav_vol);
-    qua_d.setVolume(wav_vol);
-    
-    dup_e.setVolume(wav_vol);
-    tri_e.setVolume(wav_vol);
-    qua_e.setVolume(wav_vol);
+    clv.setVolume(wav_vol);
+    rim.setVolume(wav_vol);
     
     block = knuth_shuffle(block);
 }
@@ -125,7 +88,7 @@ function setup()
 function play_sound()
 {
     trial_num += 1;
-    if (trial_num >= 15)
+    if (trial_num >= 20)
     {
         trial_num = 0;
         block = knuth_shuffle(block);
@@ -133,26 +96,9 @@ function play_sound()
     
     condition = block[trial_num];
     switch (condition)
-    {
-        case 0:  dup_a.play(); break;
-        case 1:  tri_a.play(); break;
-        case 2:  qua_a.play(); break;
-            
-        case 3:  dup_b.play(); break;
-        case 4:  tri_b.play(); break;
-        case 5:  qua_b.play(); break;
-            
-        case 6:  dup_c.play(); break;
-        case 7:  tri_c.play(); break;
-        case 8:  qua_c.play(); break;
-            
-        case 9:  dup_d.play(); break;
-        case 10: tri_d.play(); break;
-        case 11: qua_d.play(); break;
-            
-        case 12: dup_e.play(); break;
-        case 13: tri_e.play(); break;
-        case 14: qua_e.play(); break;
+    {       
+        case 3:  clv.play(); break;
+        case 4:  rim.play(); break;
     }
 }
 
@@ -160,25 +106,8 @@ function stop_sound()
 {
     switch (condition)
     {
-        case 0:  dup_a.stop(); dup_counter += 1; break;
-        case 1:  tri_a.stop(); tri_counter += 1; break;
-        case 2:  qua_a.stop(); qua_counter += 1; break;
-            
-        case 3:  dup_b.stop(); dup_counter += 1; break;
-        case 4:  tri_b.stop(); tri_counter += 1; break;
-        case 5:  qua_b.stop(); qua_counter += 1; break;
-            
-        case 6:  dup_c.stop(); dup_counter += 1; break;
-        case 7:  tri_c.stop(); tri_counter += 1; break;
-        case 8:  qua_c.stop(); qua_counter += 1; break;
-            
-        case 9:  dup_d.stop(); dup_counter += 1; break;
-        case 10: tri_d.stop(); tri_counter += 1; break;
-        case 11: qua_d.stop(); qua_counter += 1; break;
-            
-        case 12: dup_e.stop(); dup_counter += 1; break;
-        case 13: tri_e.stop(); tri_counter += 1; break;
-        case 14: qua_e.stop(); qua_counter += 1; break;
+        case 3:  clv.stop(); tri_counter += 1; break;
+        case 4:  rim.stop(); qua_counter += 1; break;
     }
 }
 
@@ -187,6 +116,7 @@ function touchStarted()
     // sound already played, waiting response
     if (mode == 0)
     {
+        // respond with 3:1
         if (mouseX > w/2+l_x-b_w/2 && mouseX < w/2+l_x+b_w/2 && mouseY > h/2+b_y-b_h/2 && mouseY < h/2+b_y+b_h/2)
         {
             clear();
@@ -194,24 +124,6 @@ function touchStarted()
             stop_sound();
             mode = 1;
             if (condition % 3 == 0)
-            {
-                dup_correct += 1;
-                right_sound.play();
-                ans_correct = 1;
-            }
-            else
-            {
-                wrong_sound.play();
-                ans_correct = 0;
-            }
-        }
-        else if (mouseX > w/2-b_w/2 && mouseX < w/2+b_w/2 && mouseY > h/2+b_y-b_h/2 && mouseY < h/2+b_y+b_h/2)
-        {
-            clear();
-            background(0);
-            stop_sound();
-            mode = 1;
-            if (condition % 3 == 1)
             {
                 tri_correct += 1;
                 right_sound.play();
@@ -223,13 +135,15 @@ function touchStarted()
                 ans_correct = 0;
             }
         }
+
+        // respond with 4:1
         else if (mouseX > w/2+r_x-b_w/2 && mouseX < w/2+r_x+b_w/2 && mouseY > h/2+b_y-b_h/2 && mouseY < h/2+b_y+b_h/2)
         {
             clear();
             background(0);
             stop_sound();
             mode = 1;
-            if (condition % 3 == 2)
+            if (condition % 3 == 1)
             {
                 qua_correct += 1;
                 right_sound.play();
@@ -262,6 +176,7 @@ function touchStarted()
 
 function highlight()
 {
+    // hover over show performance
     if (mouseX > w/2-b_w/2 && mouseX < w/2+b_w/2 && mouseY > 190 && mouseY < 210)
     {
         show_color = color(255);
@@ -271,35 +186,32 @@ function highlight()
         show_color = color(100);
     }
     
+    // waiting response
     if (mode == 0)
     {
+        // hover over 3:1
         if (mouseX > w/2+l_x-b_w/2 && mouseX < w/2+l_x+b_w/2 && mouseY > h/2+b_y-b_h/2 && mouseY < h/2+b_y+b_h/2)
         {
             l_color = color(255);
-            c_color = color(100);
             r_color = color(100);
         }
-        else if (mouseX > w/2-b_w/2 && mouseX < w/2+b_w/2 && mouseY > h/2+b_y-b_h/2 && mouseY < h/2+b_y+b_h/2)
-        {
-            l_color = color(100);
-            c_color = color(255);
-            r_color = color(100);
-        }
+        // hover over 4:1
         else if (mouseX > w/2+r_x-b_w/2 && mouseX < w/2+r_x+b_w/2 && mouseY > h/2+b_y-b_h/2 && mouseY < h/2+b_y+b_h/2)
         {
             l_color = color(100);
-            c_color = color(100);
             r_color = color(255);
         }
+        // no hover
         else
         {
             l_color = color(100);
-            c_color = color(100);
             r_color = color(100);
         }
     }
+    // ready for next
     else if (mode == 1)
     {
+        // hover over next button
         if (mouseX > w/2-b_w/2 && mouseX < w/2+b_w/2 && mouseY > h/2+2*b_y-b_h/2 && mouseY < h/2+2*b_y+b_h/2)
         {
             c_color = color(255);
@@ -315,11 +227,9 @@ function print_performance()
 {
     
     fill(255);
-    text("2:1", w/2+l_x, h/2+b_y);
-    text("3:1", w/2,     h/2+b_y);
+    text("3:1", w/2+l_x, h/2+b_y);
     text("4:1", w/2+r_x, h/2+b_y);
-    text((dup_counter.toString()).concat(" trials"), w/2+l_x, h/2+(b_y/2));
-    text((tri_counter.toString()).concat(" trials"), w/2,     h/2+(b_y/2));
+    text((tri_counter.toString()).concat(" trials"), w/2+l_x, h/2+(b_y/2));
     text((qua_counter.toString()).concat(" trials"), w/2+r_x, h/2+(b_y/2));
     
     if (trial_num != -1)
@@ -336,30 +246,20 @@ function print_performance()
         textSize(72);
         switch (condition % 3)
         {
-            case 0: text("2:1", w/2, h/2-b_y); break;
-            case 1: text("3:1", w/2, h/2-b_y); break;
-            case 2: text("4:1", w/2, h/2-b_y); break;
+            case 0: text("3:1", w/2, h/2-b_y); break;
+            case 1: text("4:1", w/2, h/2-b_y); break;
         }
         textSize(24);
     }
     
     fill(255);
-    if (dup_counter == 0)
+    if (tri_counter == 0)
     {
         text("-", w/2+l_x, h/2);
     }
     else
     {
-        text((((dup_correct/dup_counter*100).toFixed(2)).toString()).concat(" %"), w/2+l_x, h/2);
-    }
-    
-    if (tri_counter == 0)
-    {
-        text("-", w/2, h/2);
-    }
-    else
-    {
-        text((((tri_correct/tri_counter*100).toFixed(2)).toString()).concat(" %"), w/2, h/2);
+        text((((tri_correct/tri_counter*100).toFixed(2)).toString()).concat(" %"), w/2+l_x, h/2);
     }
     
     if (qua_counter == 0)
@@ -380,10 +280,11 @@ function draw()
     // show trial counter
     fill(255);
     textSize(20);
-    text(("Trials: ").concat((dup_counter+tri_counter+qua_counter).toString()), w/2, h/2-2*b_y)
+    text(("Trials: ").concat((tri_counter+qua_counter).toString()), w/2, h/2-2*b_y)
     textSize(24);
     
     // show playing symbol while sound is still active
+    /*
     switch (condition)
     {
         case 0:  if (dup_a.isPlaying()){ image(play_img, w/2, h/2-100, 128, 128); } break;
@@ -406,6 +307,7 @@ function draw()
         case 13: if (tri_e.isPlaying()){ image(play_img, w/2, h/2-100, 128, 128); } break;
         case 14: if (qua_e.isPlaying()){ image(play_img, w/2, h/2-100, 128, 128); } break;
     }
+    */
     
     // sound already played, waiting response
     if (mode == 0)
@@ -416,13 +318,7 @@ function draw()
         fill(l_color);
         quad(w/2+l_x-b_w/2,h/2+b_y+b_h/2, w/2+l_x+b_w/2,h/2+b_y+b_h/2, w/2+l_x+b_w/2,h/2+b_y-b_h/2, w/2+l_x-b_w/2,h/2+b_y-b_h/2);
         fill(text_color);
-        text("2:1", w/2+l_x, h/2+b_y);
-        
-        // center button
-        fill(c_color);
-        quad(w/2-b_w/2,h/2+b_y+b_h/2, w/2+b_w/2,h/2+b_y+b_h/2, w/2+b_w/2,h/2+b_y-b_h/2, w/2-b_w/2,h/2+b_y-b_h/2);
-        fill(text_color);
-        text("3:1", w/2, h/2+b_y);
+        text("3:1", w/2+l_x, h/2+b_y);
         
         // right button
         fill(r_color);
