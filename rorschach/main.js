8dc56c2f1cc75b46c21d7f11c936680e94ce10fe
Sebastian Waz <sebwaz@g.ucla.edu>
2017-06-03 22:53:04 -0700

    scaling in draw function
    draw function now handles scaling as well

--- rorschach/main.js
+++ rorschach/main.js
@@ -38,42 +38,99 @@ function preload()
 }
 
 // USED TO PLACE IMAGES ON CANVAS
-function draw_img(img, mir, rot, x_index, y_index)
+function draw_img(img, mir, rot, blot_size, x_index, y_index)
 {
     var x = x_index*grid_s;
     var y = y_index*grid_s;
     var x_flip = -x-grid_s;
     var y_flip = -y-grid_s;
-    if (mir == false)
+    if (blot_size == 1)
     {
-        switch(rot)
+        if (mir == false)
         {
-            case 0: /*rotate(0)*/   image(img, x,      y,      grid_s, grid_s); break;
-            case 1: rotate(PI/2);   image(img, y,      x_flip, grid_s, grid_s); break;
-            case 2: rotate(PI);     image(img, x_flip, y_flip, grid_s, grid_s); break;
-            case 3: rotate(3*PI/2); image(img, y_flip, x,      grid_s, grid_s); break;
+            switch(rot)
+            {
+                case 0: /*rotate(0)*/   image(img, x,      y,      grid_s, grid_s); break;
+                case 1: rotate(PI/2);   image(img, y,      x_flip, grid_s, grid_s); break;
+                case 2: rotate(PI);     image(img, x_flip, y_flip, grid_s, grid_s); break;
+                case 3: rotate(3*PI/2); image(img, y_flip, x,      grid_s, grid_s); break;
+            }
+        }
+        else
+        {
+            switch(rot)
+            {
+                case 0:  /*rotate(0)*/   scale (-1, 1); image(img, x_flip, y,      grid_s, grid_s); break;
+                case 1:  rotate(PI/2);   scale (-1, 1); image(img, y_flip, x_flip, grid_s, grid_s); break;
+                case 2:  rotate(PI);     scale (-1, 1); image(img, x,      y_flip, grid_s, grid_s); break;
+                case 3:  rotate(3*PI/2); scale (-1, 1); image(img, y,      x,      grid_s, grid_s); break;
+            }
         }
     }
-    else
+    else if (blot_size == 2)
     {
-        switch(rot)
+        var blot_s = grid_s*2;
+        x = x_index*grid_s;
+        y = y_index*grid_s;
+        x_flip = -x-blot_s;
+        y_flip = -y-blot_s;
+        if (mir == false)
         {
-            case 0:  /*rotate(0)*/   scale (-1, 1); image(img, x_flip, y,      grid_s, grid_s); break;
-            case 1:  rotate(PI/2);   scale (-1, 1); image(img, y_flip, x_flip, grid_s, grid_s); break;
-            case 2:  rotate(PI);     scale (-1, 1); image(img, x,      y_flip, grid_s, grid_s); break;
-            case 3:  rotate(3*PI/2); scale (-1, 1); image(img, y,      x,      grid_s, grid_s); break;
+            switch(rot)
+            {
+                case 0: /*rotate(0)*/   image(img, x,      y,      blot_s, blot_s); break;
+                case 1: rotate(PI/2);   image(img, y,      x_flip, blot_s, blot_s); break;
+                case 2: rotate(PI);     image(img, x_flip, y_flip, blot_s, blot_s); break;
+                case 3: rotate(3*PI/2); image(img, y_flip, x,      blot_s, blot_s); break;
+            }
+        }
+        else
+        {
+            switch(rot)
+            {
+                case 0:  /*rotate(0)*/   scale (-1, 1); image(img, x_flip, y,      blot_s, blot_s); break;
+                case 1:  rotate(PI/2);   scale (-1, 1); image(img, y_flip, x_flip, blot_s, blot_s); break;
+                case 2:  rotate(PI);     scale (-1, 1); image(img, x,      y_flip, blot_s, blot_s); break;
+                case 3:  rotate(3*PI/2); scale (-1, 1); image(img, y,      x,      blot_s, blot_s); break;
+            }
         }
     }
+
     resetMatrix();
     scale(1, 1);
 }
 
-// OBJECT THAT HOLDS ATTRIBUTES OF ALL POSSIBLE BLOTS
-/*
+// OBJECT THAT HOLDS ATTRIBUTES OF ALL POSSIBLE BLOTS (used in IMGS[])
 function blot(img, mir, rot, T, B, L, R)
 {
+    this.blot     = img;
+    this.mirror   = mir;
+    this.rotation = rot;
+    this.top      = T;
+    this.bottom   = B;
+    this.left     = L;
+    this.right    = R;
 }
-*/
+
+// OBJECT THAT HOLDS COVERAGE/EDGE TYPES IN GRID (used in COVER[])
+function coverage(cov, T, B, L, R)
+{
+    this.coverage = cov; 
+    this.top      = T;
+    this.bottom   = B;
+    this.left     = L;
+    this.right    = R;
+}
+
+// OBJECT THAT HOLDS ATTRIBUTES OF ACTUAL PLACED BLOT (used in PLACE[])
+function placement(add, img_index, x_i, y_i, blot_s, R)
+{
+    this.used      = add;       // indicates whether the object is being used for plotted blot (since PLACE[] is pre-allocated to max)
+    this.blot      = img_index; // reference to IMGS[] so that proper img, rotation, mirroring, can be used
+    this.x_index   = x_i;       // x index of anchor point (top left)
+    this.y_index   = y_i;       // y index of anchor point (top left)
+    this.blot_size = blot_s;    // 0, 1, or 2. indicates 1x1 or 2x2. 0 for uninitialized 
+}
 
 function setup()
 {
@@ -86,14 +143,23 @@ function draw()
 {
     clear();
     background(255);
-    draw_img(p2C_1, false, 0, 0, 0);
-    draw_img(p2C_1, false, 1, 1, 0);
-    draw_img(p2C_1, false, 2, 2, 0);
-    draw_img(p2C_1, false, 3, 3, 0);
-    draw_img(p2C_1, true, 0, 0, 1);
-    draw_img(p2C_1, true, 1, 1, 1);
-    draw_img(p2C_1, true, 2, 2, 1);
-    draw_img(p2C_1, true, 3, 3, 1);
+    draw_img(p2C_1, false, 0, 1, 0, 0);
+    draw_img(p2C_1, false, 1, 1, 1, 0);
+    draw_img(p2C_1, false, 2, 1, 2, 0);
+    draw_img(p2C_1, false, 3, 1, 3, 0);
+    draw_img(p2C_1, true, 0, 1, 0, 1);
+    draw_img(p2C_1, true, 1, 1, 1, 1);
+    draw_img(p2C_1, true, 2, 1, 2, 1);
+    draw_img(p2C_1, true, 3, 1, 3, 1);
+    draw_img(p2C_1, false, 0, 2, 0, 2);
+    draw_img(p2C_1, false, 1, 2, 2, 2);
+    draw_img(p2C_1, false, 2, 2, 4, 2);
+    draw_img(p2C_1, false, 3, 2, 6, 2);
+    draw_img(p2C_1, true, 0, 2, 0, 4);
+    draw_img(p2C_1, true, 1, 2, 2, 4);
+    draw_img(p2C_1, true, 2, 2, 4, 4);
+    draw_img(p2C_1, true, 3, 2, 6, 4);
+
     /*
     image(p2C_1, 0, grid_s, grid_s, grid_s);
     rotate(PI/2)
